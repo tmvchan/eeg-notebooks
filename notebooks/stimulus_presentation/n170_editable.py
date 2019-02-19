a79bd36c6ac06063144ed492cf4c57b011c44e3c
vchan <vchan@research.baycrest.org>
2019-02-19 16:07:09 -0500

    Edit stimulus directory in N170 notebook directly

--- notebooks/stimulus_presentation/n170_editable.py
+++ notebooks/stimulus_presentation/n170_editable.py
@@ -19,7 +19,7 @@ from psychopy import visual, core, event
 from pylsl import StreamInfo, StreamOutlet
 
 
-def present(duration=120, iti=0.8, soa=0.2, jitter=0.2):
+def present(duration=120, iti=0.8, soa=0.2, jitter=0.2, f1='face_house/faces/*_3.jpg', f2='face_house/houses/*3.jpg'):
 
     # Create markers stream outlet
     info = StreamInfo('Markers', 'Markers', 1, 0, 'int32', 'myuidw43536')
@@ -34,6 +34,8 @@ def present(duration=120, iti=0.8, soa=0.2, jitter=0.2):
     # soa = 0.2
     # jitter = 0.2
     record_duration = np.float32(duration)
+    fol1 = 'stimulus_presentation/stim/' + f1
+    fol2 = 'stimulus_presentation/stim/' + f2
 
     # Setup trial list
     image_type = np.random.binomial(1, 0.5, n_trials)
@@ -48,9 +50,9 @@ def present(duration=120, iti=0.8, soa=0.2, jitter=0.2):
     mywin = visual.Window([1600, 900], monitor='testMonitor', units='deg', winType='pygame',
                           fullscr=True)
     faces = list(map(load_image, glob(
-        'stimulus_presentation/stim/face_house/faces/*_3.jpg')))
+        fol1)))
     houses = list(map(load_image, glob(
-        'stimulus_presentation/stim/face_house/houses/*.3.jpg')))
+        fol2)))
 
     for ii, trial in trials.iterrows():
         # Intertrial interval
@@ -96,9 +98,17 @@ def main():
     parser.add_option("-j", "--jitter",
                       dest="jitter", type='float', default=0.2,
                       help="average amount of random jitter added to ITI in seconds.")
+    
+    parser.add_option("-f1", "--folder1",
+                      dest="f1", type='string', default=0.2,
+                      help="location of stimulus group 1.")
+    
+    parser.add_option("-f2", "--folder2",
+                      dest="f2", type='string', default=0.2,
+                      help="location of stimulus group 2.")
 
     (options, args) = parser.parse_args()
-    present(options.duration, options.iti, options.soa, options.jitter)
+    present(options.duration, options.iti, options.soa, options.jitter, options.f1, options.f2)
 
 
 if __name__ == '__main__':
